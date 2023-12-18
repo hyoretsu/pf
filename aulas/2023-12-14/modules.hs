22a46a9029e8968fee932dd759c0a0d279e108fc
Aran Leite <hyoretsu@gmail.com>
2023-12-17 22:58:29 -0300

    Conversão das anotações em Notebooks do Jupyter

--- aulas/2023-12-14/modules.hs
+++
@@ -1,9 +0,0 @@
-import Data.Char
-
--- Data.Char - isUpper, isLower, isLetter, toUpper, toLower
-
-countLetters :: String -> Int
-countLetters xs = length [x | x <- xs, isLetter x]
-
-strToUpper :: String -> String
-strToUpper xs = [toUpper x | x <- xs]
